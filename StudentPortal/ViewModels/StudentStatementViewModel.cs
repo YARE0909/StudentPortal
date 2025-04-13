402c295435c7c8a196722bf13151ce3e32b726e2
hawtasf <amirfarisazman05@gmail.com>
2025-04-13 20:03:39 +0800

    change layeout

---
+++ StudentPortal/ViewModels/StudentStatementViewModel.cs
@@ -0,0 +1,18 @@
+﻿// ViewModels/StudentStatementViewModel.cs
+using StudentPortal.Models;
+
+namespace StudentPortal.ViewModels
+{
+    public class StudentStatementViewModel
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+
+        public List<Enrollment> Enrollments { get; set; } = new();
+        public List<Invoice> Invoices { get; set; } = new();
+
+        public decimal TotalFinalAmount => Invoices.Sum(i => i.FinalAmount);
+        public decimal TotalPaid => Invoices.Where(i => i.Status == InvoiceStatus.Paid).Sum(i => i.FinalAmount);
+        public decimal TotalDue => Invoices.Where(i => i.Status == InvoiceStatus.Pending).Sum(i => i.FinalAmount);
+    }
+}
